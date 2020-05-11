117f34e7a3a46c30deeb9a0e323c2baef4f1c976
Abraham Mayowa Oluwasina <mayowa@Abrahams-iMac.local>
2020-05-11 14:12:17 -0700

    fix landing page optional route param

--- routes/generalRoute.js
+++ routes/generalRoute.js
@@ -9,6 +9,9 @@ router.get('/search/:page', search.searchQuery)
 
 router.get('/:page', generalControllers.goodsLists)
 
+//the second duplicate router handle url without params value for the landing page
+router.get('/', generalControllers.goodsLists)
+
 router.get('/product/:productId', generalControllers.productDetails)
 
 module.exports = router
